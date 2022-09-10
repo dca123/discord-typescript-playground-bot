9b065652118ff8d5e3a8e7c1076041ef2dc611eb
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-09-11 00:36:23 +0530

    feat: :sparkles: Working concept

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
-import LZString from 'lz-string';
+// Require the necessary discord.js classes
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import { token } from "../config.json";
+import { parse, Syntax } from "@textlint/markdown-to-ast";
+import { compressToEncodedURIComponent } from "lz-string";
 
-var string = "This is my compression test.";
-console.log("Size of sample is: " + string.length);
-var compressed = LZString.compressToEncodedURIComponent(string);
-console.log("Size of compressed sample is: " + compressed.length);
-console.log("Compressed sample is: " + compressed);
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.id === client.user?.id) {
+    return;
+  }
+
+  const result = parse(message.content);
+  const codeblocks = result.children
+    .filter((node: any) => node.type === Syntax.CodeBlock)
+    .map((node: any) => node.value);
+
+  if (codeblocks.length > 0) {
+    const code = codeblocks.join("\n");
+    const compressed = compressToEncodedURIComponent(code);
+    console.log(`https://www.typescriptlang.org/play?#code/${compressed}`);
+
+    const thread = await message.startThread({
+      name: "TypeScript Playground",
+    });
+
+    const botMessage =
+      "Here is a link to the TypeScript Playground with your code \n";
+
+    thread.send(
+      botMessage + `https://www.typescriptlang.org/play?#code/${compressed}`
+    );
+  }
+});
+
+client // Login to Discord with your client's token
+  .login(token);
